0f7057c55d783d8c85704169ad5753734f2b5d88
Kader B <kder000@gmail.com>
2023-05-30 21:03:54 +0200

    Home components added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Project</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+    </div>
+  )
+}
+
+export default Navbar
